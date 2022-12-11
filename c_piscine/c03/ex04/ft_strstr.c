e72fe089ae1955ed61886218d9c879be35943f5d
Leandro Balla <leandro.balla@outlook.com>
2022-12-11 00:37:26 +0000

    Create ft_strstr.c

---
+++ c_piscine/c03/ex04/ft_strstr.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lballa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 11:23:30 by lballa            #+#    #+#             */
+/*   Updated: 2022/12/07 16:46:00 by lballa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	tmp;
+
+	if (*to_find == '\0')
+		return (str);
+	while (*str != '\0')
+	{
+		if (*str == *to_find)
+		{
+			i = 0;
+			tmp = 0;
+			while (to_find[i] != '\0')
+			{
+				if (str[i] != to_find[i])
+					tmp = 1;
+				i++;
+			}
+			if (tmp == 0)
+				return (str);
+		}
+		str++;
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	char	*ret;
+	char	tst[] = "Hello";
+	char	find[] = "ll";
+
+	ret = ft_strstr(tst, find);
+	printf("%s", ret);
+	return (0);
+}
+*/
