847590172998018946bd1987f13cd614422154c8
Leandro Balla <leandro.balla@outlook.com>
2022-12-11 01:05:38 +0000

    Create ft_recursive_power.c

---
+++ c_piscine/c05/ex03/ft_recursive_power.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lballa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 11:56:46 by lballa            #+#    #+#             */
+/*   Updated: 2022/12/11 11:30:07 by lballa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power_recur(int nb, int power, int n)
+{
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (n);
+	if (power >= 1)
+		return (ft_recursive_power_recur(nb, power - 1, n * nb));
+	return (0);
+}
+
+int	ft_recursive_power(int nb, int power)
+{
+	return (ft_recursive_power_recur(nb, power, nb));
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_recursive_power(5, 3));
+	return (0);
+}
+*/
