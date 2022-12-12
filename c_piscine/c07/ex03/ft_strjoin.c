fb8284004ada394a33b4f64cd547977afb2c4f90
Leandro Balla <leandro.balla@outlook.com>
2022-12-12 06:27:46 +0000

    Update ft_strjoin.c

--- c_piscine/c07/ex03/ft_strjoin.c
+++ c_piscine/c07/ex03/ft_strjoin.c
@@ -6,55 +6,65 @@
 /*   By: lballa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 11:42:21 by lballa            #+#    #+#             */
-/*   Updated: 2022/12/11 12:48:17 by lballa           ###   ########.fr       */
+/*   Updated: 2022/12/12 16:52:11 by lballa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
+
 char	*ft_strejoin(int size, char **strs, char *sep)
 {
 	char	*ret;
 	int		x;
+	int		y;
 	int		r;
 
-	ret = (char *) malloc((size * 2));
-	printf("sizeRet%lu\n", sizeof (ret));
+	ret = (char *) malloc(size);
 	if (size == 0)
 	{
 		ret = NULL;
-		return ret;
+		return (ret);
 	}
 	x = 0;
+	y = 0;
 	r = 0;
-	while (x < (size - 2))
+	while (strs[x][y] != '\0')
 	{
-		//printf("%c", strs[0][x]);
-		ret[r++] = strs[0][x];
-		x++;
-		if (x != (size - 2))
+		y = 0;
+		while (strs[x][y] != '\0')
 		{
-			//printf("%c\n", sep[0]);
+			ret[r++] = strs[0][y];
+			y++;
 			ret[r++] = sep[0];
 		}
+		ret[r - 1] = '\0';
+		x++;
 	}
 	return (ret);
 }
+
 #include <stdio.h>
 int	main(void)
 {
-	char	se = ',';
-	char	*st = "abcdef";
+	char	se = ';';
+	char	*st[4];
 	char	*ret;
 	int		i;
+	int		s;
 
-	printf("size%lu\n", sizeof (st));
-	ret = ft_strejoin( sizeof (st), &st, &se);
+	st[0] = "a";
+	st[1] = "b";
+	st[2] = "c";
+	st[3] = "d";
+	s = sizeof (st);
+	printf("s%d\n", s);
+	ret = ft_strejoin( s, st, &se);
 	i = 0;
-	while (i < (sizeof (st) - 1))
+	while (ret[i] != '\0')
 	{
 		printf("%c", ret[i]);
 		i++;
 	}
 	return (0);
 }
+
